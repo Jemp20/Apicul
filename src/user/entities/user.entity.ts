28da0f464b1b675f9c088d450a9f6fc065d9fddc
joel <joelby1234@gmail.com>
2024-05-11 16:11:35 -0500

    Creando recurso user con curd

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,17 +2,19 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn()
   id: number;
   @Column()
   firstName: string;
   @Column()
   lastName: string;
   @Column()
-  Adress: string;
+  address: string;
   @Column()
-  Phonenumber: string;
-  @Column()
+  phonenumber: string;
+  @Column({
+    unique: true
+  })
   email: string;
   @Column({ default: true })
   isActive: boolean;
